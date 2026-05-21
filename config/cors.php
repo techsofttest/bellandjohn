876c037f7f3b21e7a121b1db8eb146e876b881cb
Akhil-Techsoft <techsofttest@gmail.com>
2026-05-21 10:56:01 +0530

    Cors fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'https://bell-john.vercel.app'),
+        'https://bell-john.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://127.0.0.1:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +35,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];

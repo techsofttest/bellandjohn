448fdd9e55456ceaadcb89bcbd914e097c84c2ff
Akhil-Techsoft <techsofttest@gmail.com>
2026-05-30 17:23:27 +0530

    Category tabs

--- app/Filament/Resources/Categories/Pages/ListCategories.php
+++ app/Filament/Resources/Categories/Pages/ListCategories.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\Categories\Pages;
 use App\Filament\Resources\Categories\CategoryResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\Category;
 
 class ListCategories extends ListRecords
 {
@@ -15,5 +18,22 @@ class ListCategories extends ListRecords
         return [
             CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [
+            'all' => Tab::make('All Categories')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereNull('parent_id')),
+        ];
+
+        $parentCategories = Category::whereNull('parent_id')->orderBy('name')->get();
+
+        foreach ($parentCategories as $category) {
+            $tabs['category_' . $category->id] = Tab::make($category->name)
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('parent_id', $category->id));
+        }
+
+        return $tabs;
     }
 }

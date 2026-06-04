788e10a4ca1fdac057e846cae894c46bb9fa3803
Akhil-Techsoft <techsofttest@gmail.com>
2026-06-04 11:21:27 +0530

    Remove country create option

--- app/Filament/Resources/Countries/Pages/EditCountry.php
+++ app/Filament/Resources/Countries/Pages/EditCountry.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\Countries\Pages;
 
 use App\Filament\Resources\Countries\CountryResource;
-use Filament\Actions\DeleteAction;
+//use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCountry extends EditRecord
@@ -13,7 +13,7 @@ class EditCountry extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            //DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/Countries/Pages/ListCountries.php
+++ app/Filament/Resources/Countries/Pages/ListCountries.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\Countries\Pages;
 
 use App\Filament\Resources\Countries\CountryResource;
-use Filament\Actions\CreateAction;
+//use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCountries extends ListRecords
@@ -13,7 +13,7 @@ class ListCountries extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            //CreateAction::make(),
         ];
     }
 }
